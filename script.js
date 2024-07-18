23a0a9b0d988da5e0b1ae1e55fba2201c8747bd7
Navod Lakshan <navod_2020721@fot.ruh.ac.lk>
2024-07-19 01:28:01 +0530

    Navod_19-07

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 // active hamburger menu
 let menuIcon = document.querySelector(".menu-icon");
 let navlist = document.querySelector(".navlist")
-menuIcon.addEventListener("click",()=>{
+menuIcon.addEventListener("click", () => {
     menuIcon.classList.toggle("active");
     navlist.classList.toggle("active");
     document.body.classList.toggle("open");
 });
 
 // remove navlist
-navlist.addEventListener("click",()=>{
+navlist.addEventListener("click", () => {
     navlist.classList.remove("active");
     menuIcon.classList.remove("active");
     document.body.classList.remove("open");
 })
 
 
-
 // rotate text js code
 let text = document.querySelector(".text p");
 
-text.innerHTML = text.innerHTML.split("").map((char,i)=>
+text.innerHTML = text.innerHTML.split("").map((char, i) =>
     `<b style="transform:rotate(${i * 6.3}deg")>${char}</b>`
 ).join("");
 
@@ -39,10 +38,9 @@ buttons.forEach((button, index) => {
 });
 
 
-
 // portfolio fillter
 
-var mixer = mixitup('.portfolio-gallery',{
+var mixer = mixitup('.portfolio-gallery', {
     selectors: {
         target: '.portfolio-box'
     },
@@ -61,23 +59,22 @@ var swiper = new Swiper(".mySwiper", {
         el: ".swiper-pagination",
         clickable: true,
     },
-    autoplay:{
-        delay:3000,
-        disableOnInteraction:false,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
     },
 
     breakpoints: {
-        576:{
-            slidesPerView:2,
-            spaceBetween:10,
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 10,
         },
-        1200:{
-            slidesPerView:3,
-            spaceBetween:20,
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 20,
         },
     }
 });
-
 
 
 //   skill Progress bar
@@ -86,44 +83,44 @@ const first_skill = document.querySelector(".skill:first-child");
 const sk_counters = document.querySelectorAll(".counter span");
 const progress_bars = document.querySelectorAll(".skills svg circle");
 
-window.addEventListener("scroll",()=>{
-    if(!skillsPlayed)
+window.addEventListener("scroll", () => {
+    if (!skillsPlayed)
         skillsCounter();
 })
 
 
-function hasReached(el){
+function hasReached(el) {
     let topPosition = el.getBoundingClientRect().top;
-    if(window.innerHeight >= topPosition + el.offsetHeight)return true;
+    if (window.innerHeight >= topPosition + el.offsetHeight) return true;
     return false;
 }
 
-function updateCount(num,maxNum){
+function updateCount(num, maxNum) {
     let currentNum = +num.innerText;
 
-    if(currentNum < maxNum){
+    if (currentNum < maxNum) {
         num.innerText = currentNum + 1;
-        setTimeout(()=>{
-            updateCount(num,maxNum)
-        },12)
+        setTimeout(() => {
+            updateCount(num, maxNum)
+        }, 12)
     }
 }
 
 
 let skillsPlayed = false;
 
-function skillsCounter(){
-    if(!hasReached(first_skill))return;
+function skillsCounter() {
+    if (!hasReached(first_skill)) return;
     skillsPlayed = true;
-    sk_counters.forEach((counter,i)=>{
+    sk_counters.forEach((counter, i) => {
         let target = +counter.dataset.target;
         let strokeValue = 465 - 465 * (target / 100);
 
-        progress_bars[i].style.setProperty("--target",strokeValue);
+        progress_bars[i].style.setProperty("--target", strokeValue);
 
-        setTimeout(()=>{
-            updateCount(counter,target);
-        },400)
+        setTimeout(() => {
+            updateCount(counter, target);
+        }, 400)
     });
 
     progress_bars.forEach(p => p.style.animation = "progress 2s ease-in-out forwards");
@@ -132,20 +129,20 @@ function skillsCounter(){
 
 // side progress bar
 
-let calcScrollValue = ()=>{
+let calcScrollValue = () => {
     let scrollProgress = document.getElementById("progress");
     let pos = document.documentElement.scrollTop;
 
     let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    let scrollValue = Math.round((pos * 100)/calcHeight);
+    let scrollValue = Math.round((pos * 100) / calcHeight);
 
-    if(pos > 100){
+    if (pos > 100) {
         scrollProgress.style.display = "grid";
-    }else{
+    } else {
         scrollProgress.style.display = "none";
     }
 
-    scrollProgress.addEventListener("click",()=>{
+    scrollProgress.addEventListener("click", () => {
         document.documentElement.scrollTop = 0;
     });
 
@@ -161,27 +158,28 @@ window.onload = calcScrollValue;
 let menuLi = document.querySelectorAll("header ul li a");
 let section = document.querySelectorAll('section');
 
-function activeMenu(){
+function activeMenu() {
     let len = section.length;
-    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    while (--len && window.scrollY + 97 < section[len].offsetTop) {
+    }
     menuLi.forEach(sec => sec.classList.remove("active"));
     menuLi[len].classList.add("active");
 }
+
 activeMenu();
-window.addEventListener("scroll",activeMenu);
+window.addEventListener("scroll", activeMenu);
 
 // scroll reveal
 
 ScrollReveal({
-    distance:"90px",
-    duration:2000,
-    delay:200,
+    distance: "90px",
+    duration: 2000,
+    delay: 200,
     // reset: true ,
 });
 
 
-ScrollReveal().reveal('.hero-info,.main-text,.proposal,.heading', { origin: "top" });
-ScrollReveal().reveal('.about-img,.fillter-buttons,.contact-info', { origin: "left" });
-ScrollReveal().reveal('.about-content,.skills', { origin: "right" });
-ScrollReveal().reveal('.allServices,.portfolio-gallery,.blog-box,footer,.img-hero', { origin: "bottom" });
-
+ScrollReveal().reveal('.hero-info,.main-text,.proposal,.heading', {origin: "top"});
+ScrollReveal().reveal('.about-img,.fillter-buttons,.contact-info', {origin: "left"});
+ScrollReveal().reveal('.about-content,.skills', {origin: "right"});
+ScrollReveal().reveal('.allServices,.portfolio-gallery,.blog-box,footer,.img-hero', {origin: "bottom"});
